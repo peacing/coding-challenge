28ad3ce4a6a4e430a028b706d9669edc14ba198e
psingman <paulesingman@gmail.com>
2016-07-10 12:28:33 -0400

    remove file args from script

--- run.sh
+++ run.sh
@@ -4,6 +4,4 @@
 
 # I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
 
- python ./src/process_payments.py --input_file ./venmo_input/venmo-trans.txt --output_file ./venmo_output/output.txt
-
-
+python ./src/process_payments.py
