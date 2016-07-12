6233c3569a2e3fc5e9ce8d2da715098dedd5f30e
psingman <paulesingman@gmail.com>
2016-07-11 23:29:43 -0400

    remove print statements

--- src/process_payments.py
+++ src/process_payments.py
@@ -2,37 +2,31 @@
 
 import json
 import argparse
+
 from os import path
 from calendar import timegm
 from time import strptime
+
 from rolling_median import PaymentGraph
 
 
 class VenmoPayment(object):
-    """ Formats tweet text to correct format to be added to the payment graph.
+    """ Formats payment text to correct format to be added to the payment graph.
 
-        timestamp : {int} timestamp of {str} "created_at" attribute of tweet
-            See: http://stackoverflow.com/questions/9637838/convert-string-date-to-timestamp-in-python
-
-        hashtags : {set} of hashtags to be added to hashtag graph. Taken from {list}
-            of hashtags from original tweet. Requires two or more hashtags per tweet,
-            else is empty set.
+    :param target : {str} value from target key of payment dictionary
+    :param actor : {str} value from actor key of payment dictionary
+    :param created_time : {str} value from created time key of payment dictionary
+    :returns None
     """
 
     def __init__(self, target, actor, created_time):
-        """Format payment fields.
-
-        Parameters
-        ----------
-
-        created_time : {str} of datetime information
-        hashtags: {list} containing hashtags mentioned in tweet
-        """
+        # remove the T and Z from created time string, then convert to timestamp
         self.timestamp = created_time.replace('T',' ').rstrip('Z')
         self.timestamp = timegm(strptime(self.timestamp, "%Y-%m-%d %H:%M:%S"))
         # no formatting/validation done to target or actor fields
         self.target = target
         self.actor = actor
+
 
 def parse_payment_input(payment_dict):
 
@@ -41,7 +35,6 @@ def parse_payment_input(payment_dict):
     actor = payment_dict["actor"]
 
     return created_time, target, actor
-
 
 
 if __name__ == '__main__':
@@ -68,7 +61,7 @@ if __name__ == '__main__':
     # open output file
     output = open(path.abspath(output_file_path), 'w')
 
-    # initialize PaymentGraph instance (from median_degree.py)
+    # initialize PaymentGraph instance (from median_degree)
     payment_graph = PaymentGraph()
 
     # read in tweets from input file
@@ -90,13 +83,12 @@ if __name__ == '__main__':
 
                 # calculated average degree of updated graph
                 median_degree = payment_graph.calculate_median_degree()
-                #print("median_degree: {}\n".format(median_degree))
 
                 # write average degree to output txt file
                 output.write("{}\n".format(median_degree))
 
             except Exception as e:
 
-                pass  # just letting it go
+                pass  # given input seems consistent but good not to take chances and pass errors
 
     output.close()
